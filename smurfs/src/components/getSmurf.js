cf706abee13b2b8ed85d22655afc69ca2cc44d5b
Anthony Johnson <wontons713@Anthonys-MacBook-Pro.local>
2020-02-21 13:08:39 -0700

    getSmurf component finished

--- smurfs/src/components/getSmurf.js
+++ smurfs/src/components/getSmurf.js
@@ -4,23 +4,28 @@ import { useState, useEffect } from 'react';
 
 function GetSmurf() {
     const [smurf, setSmurf] = useState([]);
-      useEffect(() => {
-        axios.get("http://localhost:3333/smurfs")
-        .then(res => {
-          console.log('res',res);
-          setSmurf(res.data);
+        useEffect(() => {
+            axios.get("http://localhost:3333/smurfs")
+            .then(res => {
+            console.log('res',res);
+            setSmurf(res.data);
+            })
+            .catch(error => {
+            console.log("the data was not returned", error)
         })
-        .catch(error => {
-          console.log("the data was not returned", error)
-        })
-      },[])
-      console.log('smurf', smurf)
-      return (
-          <div>
-              {smurf.map((smurf) => {
-                <div className='getsmurf'>{smurf}</div>
-             })}
-          </div>
+        },[])
+        console.log('smurf', smurf)
+        return (
+            <div>
+              {smurf.map((smurf) => (
+                <div className='getSmurf' key={smurf.id}>
+                <div className='getName'>Name: {smurf.name}</div>
+                <div className='getAge'>Age: {smurf.age}</div>
+                <div className='getHeight'>Height: {smurf.height}</div>
+                </div>
+              ))}
+            </div>
+            
       )
       
      
